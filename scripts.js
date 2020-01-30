2529aa2130035aabe300ce712dd3ca679bfa2bdb
marselgray <marselgray@gmail.com>
2020-01-30 17:13:08 -0500

    Update: final operators

--- scripts.js
+++ scripts.js
@@ -1,24 +1,22 @@
-
-// functions
+// functions 
 function getHistory(){
-	return document.getElementById('history-value').innerText;
+	return document.getElementById("history-value").innerText;
 }
 
-function printHistory(num) {
-	document.getElementById('history-value').innerText = num;
+function printHistory(num){
+	document.getElementById("history-value").innerText=num;
 }
 
-function getOutput(num) {
-	document.getElementById('output-value').innerText = num;
+function getOutput(){
+	return document.getElementById("output-value").innerText;
 }
 
-function printOutput(num) {
-	if( num == '' ) {
-		document.getElementById('output-value').innerText = num;
-	} else {
-		document.getElementById('output-value').innerText = getFormattedNumber(num);
-	}
-
+function printOutput(num){
+	if(num==""){
+		document.getElementById("output-value").innerText=num;
+	} else{
+		document.getElementById("output-value").innerText=getFormattedNumber(num);
+	}	
 }
 
 /*
@@ -28,55 +26,79 @@ Read More at:
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
 */
 
-function getFormattedNumber(num) {
+
+function getFormattedNumber(num){
+	if(num=="-"){
+		return "";
+	}
+
 	var n = Number(num);
-	var value = n.toLocaleString( 'en' );
+	var value = n.toLocaleString("en");
 	return value;
 }
 
-function reverseNumberFormat(num) {
+function reverseNumberFormat(num){
 	return Number(num.replace(/,/g,''));
 }
 
-
 // opreations
-var operator = document.getElementsByClassName('operator');
+var operator = document.getElementsByClassName("operator");
 
-for (let i = 0; i < operator.length; i++) {
-	operator[i].addEventListener('click', function() {
+for(var i =0;i<operator.length;i++){
+	operator[i].addEventListener('click',function(){
 
 		// clear history
-		if (this.id == 'clear'){
-			printHistory('');
-			printOutput('');
+		if(this.id=="clear"){
+			printHistory("");
+			printOutput("");
 		}
-
 		// backspace clear
-		if (this.id == 'backspace'){
-			var output = reverseNumberFormat(getOutput()).toString();
-			
-			// if output has value
-			if (output){
-				output = output.substr(0, output.length - 1);
+		else if(this.id=="backspace"){
+			var output=reverseNumberFormat(getOutput()).toString();
+			if(output){
+				//if output has a value
+				output= output.substr(0,output.length-1);
 				printOutput(output);
 			}
+		} else{
+			var output=getOutput();
+			var history=getHistory();
+			
+			if(output==""&&history!=""){
+				if(isNaN(history[history.length-1])){
+					history= history.substr(0,history.length-1);
+				}
+			}
+
+			if(output!="" || history!=""){
+				output= output==""?output:reverseNumberFormat(output);
+				history=history+output;
+
+				if(this.id=="="){
+					var result=eval(history);
+					printOutput(result);
+					printHistory("");
+				} else{
+					history=history+this.id;
+					printHistory(history);
+					printOutput("");
+				}
+			}
 		}
-
-	})
+		
+	});
 }
+var number = document.getElementsByClassName("number");
 
-var number = document.getElementsByClassName('number');
+for(var i =0;i<number.length;i++){
+	number[i].addEventListener('click',function(){
+		
+		var output=reverseNumberFormat(getOutput());
 
-for (let i = 0; i < number.length; i++) {
-	number[i].addEventListener('click', function() {
-		var output = reverseNumberFormat(getOutput());
-
-		// if output is a number
-		if( output != NaN){
-			output = output+this.id;
+		//if output is a number
+		if(output!=NaN){
+			output=output+this.id;
 			printOutput(output);
 		}
-	})
+	});
 }
-
-
