19ae67fa0a4331823272c7314b41e17a45cb70ac
marselgray <marselgray@gmail.com>
2020-01-29 19:36:03 -0500

    Update: added first operations

--- scripts.js
+++ scripts.js
@@ -1,18 +1,24 @@
 
+// functions
 function getHistory(){
 	return document.getElementById('history-value').innerText;
 }
 
-function printHistory(num){
+function printHistory( num ) {
 	document.getElementById('history-value').innerText = num;
 }
 
-function getOutput(num){
+function getOutput( num ) {
 	document.getElementById('output-value').innerText = num;
 }
 
-function printOutput(num){
-	document.getElementById('output-value').innerText = getFormattedNumber(num);
+function printOutput( num ) {
+	if( num == '' ) {
+		document.getElementById('output-value').innerText = num;
+	} else {
+		document.getElementById('output-value').innerText = getFormattedNumber(num);
+	}
+
 }
 
 /*
@@ -22,8 +28,24 @@ Read More at:
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
 */
 
-function getFormattedNumber(num){
-	var n = Number(num);
-	var value = n.toLocaleString('en');
+function getFormattedNumber( num ) {
+	var n = Number( num );
+	var value = n.toLocaleString( 'en' );
 	return value;
+}
+
+
+function reverseNumberFormat( num ) {
+	return Number( num.replace(/,/g,''));
+}
+
+
+
+// opreations
+var operator = document.getElementsByClassName('operator');
+
+for (let i = 0; i < operator.length; i++){
+	operator[i].addEventListener('click', function(){
+		console.log(`the operator click:${this.id}`)
+	})
 }
